6144eb638e858e47014274aa3475fe19fa457c1a
Anish Susarla <47859534+anishs37@users.noreply.github.com>
2022-08-10 08:16:02 -0400

    Simple linear regression

--- app/src/main/java/com/susarlaanish/ml_demo/MainActivity.java
+++ app/src/main/java/com/susarlaanish/ml_demo/MainActivity.java
@@ -2,13 +2,60 @@ package com.susarlaanish.ml_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
+    EditText input_tv;
+    Button predict_bt;
+    TextView output_tv;
+    Interpreter  interpreter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input_tv = findViewById(R.id.input_tv);
+        predict_bt = findViewById(R.id.predict_bt);
+        output_tv = findViewById(R.id.output_tv);
+        try {
+            interpreter = new Interpreter(loadModelFile(), null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        predict_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float f = doInference(input_tv.getText().toString());
+                output_tv.setText("result: " + f);
+            }
+        });
+    }
+
+    private MappedByteBuffer loadModelFile() throws IOException {
+        AssetFileDescriptor assetFileDescriptor = this.getAssets().openFd("linreg.tflite");
+        FileInputStream fileInputStream = new FileInputStream(assetFileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = fileInputStream.getChannel();
+        long startOffset = assetFileDescriptor.getStartOffset();
+        long length = assetFileDescriptor.getLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, length);
+    }
+
+    public float doInference(String val){
+        float[] input = new float[1];
+        input[0] = Float.parseFloat(val);
+        float[][] output = new float[1][1];
+        interpreter.run(input, output);
+        return output[0][0];
     }
 }
